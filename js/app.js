f3844bb43372aacb569113c1daa580337feb3435
Kevin Briggs <kbriggs@aapl000745.local>
2016-04-15 10:41:14 -0400

    displaying enemy

--- js/app.js
+++ js/app.js
@@ -6,31 +6,58 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = 100;
+    this.y = 100; 
 };
+
+
+
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
+
+
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
 };
 
+
+
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var player = function(){
+    this.sprite = 'image/char-boy.png'; 
+    this.x = 200;
+    this.y = 300;
+
+};
+
+player.prototype.update = function(dt){
+    console.log('update');
+};
+
+player.prototype.render = function(){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-
+var enemy1 = new Enemy();
+allEnemies = [enemy1];
+var player = new player;
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
